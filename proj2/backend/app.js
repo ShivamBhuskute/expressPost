a552fb86ba34548b64db9012fdca3192104237d0
ShivamBhuskute <bhuskuteshivam8@gmail.com>
2024-08-01 11:03:20 +0530

    routes in express

--- proj2/backend/app.js
+++ proj2/backend/app.js
@@ -2,9 +2,11 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("<h1>Hii!!!</h1>");
-});
+// app.get("/", (req, res) => {
+//     res.send("<h1>Hii!!!</h1>");
+// });
+
+app.use("/", (req, res) => res.send("<h1>Hello! world</h1>"));
 
 app.listen(8000, () => {
     console.log(`Server is listening at http://localhost:8000`);
